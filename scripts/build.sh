ac8c880dadf4f12aa88c75545a43961eacc99254
Andreas Salhus Bakseter <141913422+baksetercx@users.noreply.github.com>
2024-08-29 11:03:17 +0200

    checksum & env

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,12 +7,7 @@ main() {
     local binary_path="$out_dir/3lv-linux-amd64"
     local compressed_package_path="$binary_path.tar.gz"
 
-    export GO111MODULE=on
-    export CGO_ENABLED=0
-    export GOOS=linux
-    export GOARCH=amd64
-
-    go build -o "$binary_path" ./cmd/3lv
+    env GO111MODULE=on CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o "$binary_path" ./cmd/3lv
     echo "$binary_path"
 
     if [[ "$2" == "--compress" ]]; then
