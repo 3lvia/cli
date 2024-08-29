172310800723dde7cb36c92ab0de5f97b11dd92b
Andreas Salhus Bakseter <141913422+baksetercx@users.noreply.github.com>
2024-08-29 15:26:20 +0200

    Add new build arguments

--- pkg/build/build.go
+++ pkg/build/build.go
@@ -9,6 +9,67 @@ import (
 	"github.com/urfave/cli/v2"
 )
 
+var Command *cli.Command = &cli.Command{
+	Name:    "build",
+	Aliases: []string{"b"},
+	Usage:   "Build the project",
+	Flags: []cli.Flag{
+		&cli.StringFlag{
+			Name:    "project-file",
+			Aliases: []string{"f"},
+			Usage:   "The project file to use",
+		},
+		&cli.StringFlag{
+			Name:    "build-context",
+			Aliases: []string{"c"},
+			Usage:   "The build context to use",
+		},
+		&cli.StringFlag{
+			Name:    "system-name",
+			Aliases: []string{"s"},
+			Usage:   "The system name to use",
+		},
+		&cli.StringFlag{
+			Name:    "registry",
+			Aliases: []string{"r"},
+			Usage:   "The registry to use",
+			Value:   "acr",
+		},
+		&cli.StringFlag{
+			Name:  "go-main-package-directory",
+			Usage: "The main package directory to use when building a Go application",
+		},
+		&cli.StringFlag{
+			Name:  "cache-tag",
+			Usage: "The cache tag to use",
+			Value: "latest-cache",
+		},
+		&cli.StringFlag{
+			Name:    "severity",
+			Aliases: []string{"S"},
+			Usage:   "The severity to use when scanning the image: can be any combination of CRITICAL, HIGH, MEDIUM, LOW, or UNKNOWN separated by commas",
+			Value:   "CRITICAL,HIGH",
+		},
+		&cli.StringSliceFlag{
+			Name:    "include-files",
+			Aliases: []string{"i"},
+			Usage:   "The files to include in the build context",
+		},
+		&cli.StringSliceFlag{
+			Name:    "include-directories",
+			Aliases: []string{"I"},
+			Usage:   "The directories to include in the build context",
+		},
+		&cli.BoolFlag{
+			Name:    "push",
+			Aliases: []string{"p"},
+			Usage:   "Push the image to the registry",
+			Value:   false,
+		},
+	},
+	Action: Build,
+}
+
 func Build(c *cli.Context) error {
 	if c.NArg() <= 0 {
 		return cli.Exit("No input provided", 1)
@@ -24,11 +85,12 @@ func Build(c *cli.Context) error {
 		return cli.Exit("Project file not provided", 1)
 	}
 	buildContext := c.String("build-context")
-	includeFiles := c.StringSlice("include-files")
-	includeDirectories := c.StringSlice("include-directories")
 	goMainPackageDirectory := c.String("go-main-package-directory")
 	systemName := c.String("system-name")
 	registry := c.String("registry")
+	severity := c.String("severity")
+	includeFiles := c.StringSlice("include-files")
+	includeDirectories := c.StringSlice("include-directories")
 	push := c.Bool("push")
 
 	generateOptions := GenerateDockerfileOptions{
@@ -51,6 +113,7 @@ func Build(c *cli.Context) error {
 		ApplicationName: applicationName,
 		SystemName:      systemName,
 		Registry:        registry,
+		Severity:        severity,
 		Push:            push,
 	}
 
@@ -65,10 +128,12 @@ func Build(c *cli.Context) error {
 type BuildAndPushImageOptions struct {
 	DockerfilePath  string
 	BuildContext    string
+	CacheTag        string
 	ApplicationName string
 	SystemName      string
 	Registry        string
-	Tags            []string
+	Severity        string
+	AdditionalTags  []string
 	Push            bool
 }
 
@@ -76,11 +141,16 @@ func buildAndPushImage(options BuildAndPushImageOptions) error {
 	registry := getRegistry(options.Registry)
 	imageName := registry + "/" + options.SystemName + "-" + options.ApplicationName
 
+	tags := func() []string {
+		if len(options.AdditionalTags) == 0 {
+			return []string{options.CacheTag}
+		}
+
+		return append(options.AdditionalTags, options.CacheTag)
+	}()
+
 	var tagArguments []string
-	if len(options.Tags) == 0 {
-		options.Tags = []string{"latest-cache", "latest"}
-	}
-	for _, tag := range options.Tags {
+	for _, tag := range tags {
 		tagArguments = append(tagArguments, "-t")
 		tagArguments = append(tagArguments, imageName+":"+tag)
 	}
@@ -103,6 +173,14 @@ func buildAndPushImage(options BuildAndPushImageOptions) error {
 		return fmt.Errorf("Failed to build Docker image: %w", err)
 	}
 
+	err := scanImage(ScanImageOptions{
+		ImageName: imageName,
+		Severity:  options.Severity,
+	})
+	if err != nil {
+		return err
+	}
+
 	if options.Push {
 		/*
 			err := loginToRegistry(options.Registry)
@@ -123,11 +201,6 @@ func buildAndPushImage(options BuildAndPushImageOptions) error {
 		if err := pushCmd.Run(); err != nil {
 			return fmt.Errorf("Failed to push Docker image: %w", err)
 		}
-	}
-
-	err := scanImage(imageName)
-	if err != nil {
-		return err
 	}
 
 	return nil
@@ -170,13 +243,18 @@ func getRegistry(registry string) string {
 	return registry
 }
 
-func scanImage(imageName string) error {
+type ScanImageOptions struct {
+	ImageName string // required
+	Severity  string // required
+}
+
+func scanImage(options ScanImageOptions) error {
 	scanCmd := exec.Command(
 		"trivy",
 		"image",
 		"--severity",
-		"CRITICAL,HIGH",
-		imageName,
+		options.Severity,
+		options.ImageName,
 	)
 	scanCmd.Stdout = os.Stdout
 	scanCmd.Stderr = os.Stderr
