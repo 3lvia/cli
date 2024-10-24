860fbdf7ec7184c0be95ce572ffc927976f8b575
Andreas Salhus Bakseter <141913422+baksetercx@users.noreply.github.com>
2024-10-24 16:27:50 +0200

    Don't run gcloud auth login command (#92)
    * Don't run gcloud auth login command
    
    * v0.13.1

--- pkg/auth/googlecloud.go
+++ pkg/auth/googlecloud.go
@@ -1,21 +1,17 @@
 package auth
 
-import (
-	"fmt"
-	"os/exec"
-
-	"github.com/3lvia/cli/pkg/command"
-)
-
 func AuthenticateGoogle() error {
-	gcloudAuthLoginOutput := gcloudAuthLoginCommand(nil)
-	if command.IsError(gcloudAuthLoginOutput) {
-		return fmt.Errorf("Failed to authenticate to Google Cloud: %w", gcloudAuthLoginOutput.Error)
-	}
+	/*
+		gcloudAuthLoginOutput := gcloudAuthLoginCommand(nil)
+		if command.IsError(gcloudAuthLoginOutput) {
+			return fmt.Errorf("Failed to authenticate to Google Cloud: %w", gcloudAuthLoginOutput.Error)
+		}
+	*/
 
 	return nil
 }
 
+/*
 func gcloudAuthLoginCommand(
 	runOptions *command.RunOptions,
 ) command.Output {
@@ -28,3 +24,4 @@ func gcloudAuthLoginCommand(
 		runOptions,
 	)
 }
+*/
