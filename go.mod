fafaae83762e211a29f2c7120cf57911db59d757
Andreas Salhus Bakseter <141913422+baksetercx@users.noreply.github.com>
2024-12-05 16:04:12 +0100

    CORE-2177 add template for dotnet8-worker

--- go.mod
+++ go.mod
@@ -20,6 +20,7 @@ require (
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.16 // indirect
 	github.com/muesli/termenv v0.15.2 // indirect
+	github.com/orsinium-labs/enum v1.4.0 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
 	golang.org/x/sys v0.27.0 // indirect
 )
--- tests/create/run_tests.sh
+++ tests/create/run_tests.sh
@@ -2,33 +2,43 @@
 
 set -e
 
-test_create_dotnet() {
-    output_dir="$(mktemp -d)"
+test_create_dotnet8() {
     app_name='demo-api'
     project_dir='DemoApi' # pascal case for .NET
 
-    if ! 3lv create \
-        -s core \
-        -a "$app_name" \
-        -t dotnet \
-        "$output_dir"; then
-        echo "Failed to create project"
-        exit 1
-    fi
+    for dotnet8_template_type in dotnet8-webapi dotnet8-worker; do
+        output_dir="$(mktemp -d)"
 
-    if [[ ! -d "$output_dir/$project_dir" ]]; then
-        echo "Project directory does not exist"
-        exit 1
-    fi
+        if ! 3lv create \
+            -s core \
+            -a "$app_name" \
+            -t "$dotnet8_template_type" \
+            --non-interactive \
+            "$output_dir"; then
+            echo "Failed to create project with template $dotnet8_template_type."
+            exit 1
+        fi
 
-    if [[ ! -f "$output_dir/$project_dir/.github/workflows/build-deploy-$app_name.yml" ]]; then
-        echo "Workflow file does not exist"
-        exit 1
-    fi
+        if [[ ! -d "$output_dir/$project_dir" ]]; then
+            echo "Project directory does not exist for template $dotnet8_template_type."
+            exit 1
+        fi
+
+        if [[ ! -f "$output_dir/$project_dir/.github/workflows/build-deploy-$app_name.yml" ]]; then
+            echo "Workflow file does not exist for template $dotnet8_template_type."
+            exit 1
+        fi
+
+        # TODO: use 3lv build
+        if ! dotnet build "$output_dir/$project_dir"; then
+            echo "Failed to build project for template $dotnet8_template_type."
+            exit 1
+        fi
+    done
 }
 
 main() {
-    test_create_dotnet
+    test_create_dotnet8
 
     echo 'All tests passed!'
 }
