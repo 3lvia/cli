642646f1e5b64d2bc8951dfff55ce2203961e092
Andreas Salhus Bakseter <141913422+baksetercx@users.noreply.github.com>
2024-12-09 16:31:19 +0100

    Don't run version check after upgrading

--- main.go
+++ main.go
@@ -14,10 +14,10 @@ import (
 	"github.com/3lvia/cli/pkg/githubactions"
 	"github.com/3lvia/cli/pkg/run"
 	"github.com/3lvia/cli/pkg/scan"
+	"github.com/3lvia/cli/pkg/shared"
 	"github.com/3lvia/cli/pkg/style"
 	"github.com/3lvia/cli/pkg/upgrade"
 	"github.com/urfave/cli/v3"
-	"golang.org/x/mod/semver"
 )
 
 //go:embed VERSION
@@ -32,6 +32,19 @@ func main() {
 	}
 	version := strings.TrimSpace(string(versionFile))
 
+	// Check for a new version of the CLI after all these commands.
+	// Any new commands should be added here.
+	commands := shared.WithCheckVersionAfterCommands(
+		[]*cli.Command{
+			build.Command,
+			deploy.Command,
+			scan.Command,
+			githubactions.Command,
+			run.Command,
+			create.Command,
+		},
+	)
+
 	app := &cli.Command{
 		Name:                  "3lv",
 		Usage:                 "Command Line Interface tool for developing, building and securing Elvia applications ⚡",
@@ -44,27 +57,8 @@ func main() {
 				Sources: cli.EnvVars("3LV_NON_INTERACTIVE"),
 			},
 		},
-		After: func(ctx context.Context, c *cli.Command) error {
-			latestVersion, _ := upgrade.GetLatestCLIVersion(ctx)
-			if semver.Compare("v"+version, "v"+latestVersion) == -1 {
-				style.Print(
-					fmt.Sprintf("\n\nA new version of 3lv is available! %s -> %s", version, latestVersion),
-					&style.PrintOptions{Color: "yellow"},
-				)
-			}
-
-			return nil
-		},
-		Commands: []*cli.Command{
-			build.Command,
-			deploy.Command,
-			scan.Command,
-			githubactions.Command,
-			run.Command,
-			create.Command,
-			// special case since it needs the version
-			upgrade.Command(version),
-		},
+		// Don't check for updates when running the upgrade command.
+		Commands: append(commands, upgrade.Command(version)),
 	}
 
 	ctx := context.Background()
