cbee752dc18c70f1aefe4fd9f507efabc537f034
Andreas Salhus Bakseter <141913422+baksetercx@users.noreply.github.com>
2025-01-21 15:34:49 +0100

    Fix using two tags when running docker inspect

--- pkg/deploy/deploy_test.go
+++ pkg/deploy/deploy_test.go
@@ -4,13 +4,14 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/3lvia/cli/pkg/build"
 	"github.com/3lvia/cli/pkg/command"
 )
 
 func TestDockerInspectCommand(t *testing.T) {
 	t.Parallel()
 
-	const imageName = "ghcr.io/3lvia/core/demo-api"
+	const imageName = "ghcr.io/3lvia/core/demo-api" + build.DefaultCacheTag
 
 	expectedCommandString := strings.Join(
 		[]string{
@@ -18,7 +19,7 @@ func TestDockerInspectCommand(t *testing.T) {
 			"inspect",
 			"--format",
 			"{{index .RepoDigests 0}}",
-			imageName + ":latest-cache",
+			imageName,
 		},
 		" ",
 	)
