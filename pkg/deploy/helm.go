7899bdb26773c7362b8e0e3b16ca71378fd6f135
Rune Rystad <rune.rystad@elvia.no>
2026-09-24 15:42:53 +0200

    Justere formattering

--- pkg/deploy/helm.go
+++ pkg/deploy/helm.go
@@ -35,12 +35,14 @@ func helmDeployCommand(
 	useISSChart bool,
 	runOptions *command.RunOptions,
 ) command.Output {
-	if workloadType != "deployment" && workloadType != "statefulset" 
-		&& workloadType != "job" && workloadType != "cronjob" {
-
+	switch workloadType {
+	case "deployment", "statefulset", "job", "cronjob":
+	default:
 		return command.Error(
-			fmt.Errorf("Workload type must be either deployment, statefulset, cronjob or job, got '%s'.", 
-				workloadType),
+			fmt.Errorf(
+				"Workload type must be either deployment, statefulset, cronjob or job, got '%s'.",
+				workloadType,
+			),
 		)
 	}
 
